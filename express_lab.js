6cd6157290106b623bb3b1aeb616a9bc056fbc23
Chris <chewning.cl@gmail.com>
2018-07-09 13:58:40 -0500

    Commit 1: Getting Express Application Started

--- express_lab.js
+++ express_lab.js
@@ -2,16 +2,36 @@
 //LINK: https://git.generalassemb.ly/WebDev-Connected-Classroom/express-lab-params/blob/master/README.md
 
 
-//Greetings App
-app.get('/greeting/:name', (req, res) => {
-  res.send({params: req.params, queries: req.query})
-});
 
+//STEP 1: Get the browser to say the name variable.
+// const express = require('express');
+// const app = express(); //app stands for application
+// const name = 'Chris';
+//
+// //#1: Greetings App
+// app.get("/greeting/:name", (req, res) => {
+//   res.send(req.params.name)
+// });
+//
+// app.listen(5000, () => {
+//   console.log("listening on port 5000");
+// });
+
+// http://localhost:5000/greeting/Chris will say Chris in the browser
+
+
+//STEP 2: Get the browser to say a sentence with the name variable.
+// http://localhost:5000/greeting/Chris will say Hello there Chris in the browser
 
 const express = require('express');
-const app = express();
+const app = express(); //app stands for application
+const name = 'Chris';
 
+//#1: Greetings App
+app.get("/greeting/:name", (req, res) => {
+  res.send('Hello there ' + req.params.name)
+});
 
 app.listen(5000, () => {
-  console.log('listeining on port 5000');
+  console.log("listening on port 5000");
 });
